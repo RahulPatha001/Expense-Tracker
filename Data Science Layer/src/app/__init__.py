0e2e904a1c3f733c9eb0fca9b5ef3b69ab9670f2
RahulPatha001 <rahul.patha29@gmail.com>
2025-06-30 21:37:24 +0530

    ci: integreated kafka in data science service

--- Data Science Layer/src/app/__init__.py
+++ Data Science Layer/src/app/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask import request, jsonify
 from service.messageService import MessageService
+from kafka import KafkaProducer
+import json
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
 messageService = MessageService()
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'], value_serializer= lambda v: json.dumps(v).encode('utf-8'))
 
 @app.route('/v1/message/', methods=['POST'])
 def handle_message():
